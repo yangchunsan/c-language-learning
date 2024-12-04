05642b6cdd17354b47f6cfa13a46f3af5ab567f4
HengRui Li <403056885@qq.com>
2024-12-05 07:55:17 +0800

    2024/12/05 commit

---
+++ 2.1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int num = 1,abc = 2;
+	
+	printf("I am a simple computer");/*使用一个printf()函数*/
+    printf(".\n");
+	printf("My favorite number is %d %d because it is first.\n",num,abc);
+	
+	return 0;
+}  
