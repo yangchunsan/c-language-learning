12c319a545d76fd383b98a408df81d321d10a225
HengRui Li <403056885@qq.com>
2024-12-29 23:08:57 +0800

    2024/12/29 commit

---
+++ Program/5.3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int jane = 68;
+int tarzan = 68;
+int cheeta = 68;
+ 
+int main()
+{
+	printf("                   cheeta  tarzan  jane\n");
+	printf("First roundscore %4d %8d %8d\n", cheeta, tarzan, jane);
+	
+	return 0;
+}
