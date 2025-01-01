9bb395e9305c53ba48233802efef61e9720c7336
HengRui Li <403056885@qq.com>
2025-01-01 23:30:40 +0800

    2025/1/1 commit

---
+++ Program/5.6/5.6/源.cpp
@@ -0,0 +1,10 @@
+//ÑÝÊ¾³ý·¨
+#include <stdio.h>
+int main()
+{
+	printf("integer division:  5/4  is %d \n", 5 / 4);
+	printf("integer division:  7/4  is %d \n", 7 / 4);
+	printf("floating division:  7./4  is %f  \n", 7. / 4);
+
+	return 0;
+}
